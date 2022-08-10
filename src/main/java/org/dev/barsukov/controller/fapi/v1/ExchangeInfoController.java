b090eb8db434accd8dcf1b761d538b4cee4e7511
Barsukov Nikita <n.barsukov@abr.ru>
2022-08-10 23:12:31 +0300

    exchangeInfo Done

---
+++ src/main/java/org/dev/barsukov/controller/fapi/v1/ExchangeInfoController.java
@@ -0,0 +1,35 @@
+package org.dev.barsukov.controller.fapi.v1;
+
+import io.swagger.annotations.ApiOperation;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.dev.barsukov.api.client.BinanceClient;
+import org.dev.barsukov.service.TradeService;
+import org.dev.barsukov.service.dto.TradeDto;
+import org.hibernate.annotations.Cache;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@AllArgsConstructor
+@RestController
+@CacheConfig(cacheNames = "exchangeInfos")
+@RequestMapping("/fapi/v1/exchangeInfo")
+public class ExchangeInfoController {
+    private final TradeService service;
+    private final BinanceClient binanceClient;
+
+    @ApiOperation(value = "Gets trades by sessionId and symbol.")
+    @GetMapping()
+    @Cacheable
+    public String getExchangeInfo() {
+        return binanceClient.getExchangeInfo();
+    }
+}
